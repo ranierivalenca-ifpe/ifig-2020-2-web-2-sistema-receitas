a7235d0d0fc373895f3d4602d0708762dd1222a8
Ranieri Valença <ranierivalenca@gmail.com>
2021-01-07 15:26:04 -0300

    Login / auth sytem

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ooooi', function() {
-    return view('oi');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
