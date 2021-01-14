616ee389d18bf20ae00e4660beb7709ec1ddac58
Ranieri Valença <ranierivalenca@gmail.com>
2021-01-14 14:32:15 -0300

    Adding UserSeeder, emails are now like 'user0@email.com'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
         $this->call([
+            UserSeeder::class,
             ProdutoSeeder::class,
             ReceitaSeeder::class,
             ProdutoReceitaSeeder::class,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            User::factory()->create([
+                'email' => 'user' . $i . '@email.com'
+            ]);
+        }
+    }
+}
